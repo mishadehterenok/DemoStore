3eff07349f51c70003b0b68f8e7b6247af9e749d
mishadehterenok <m.dhtrnk@gmail.com>
2021-09-06 23:05:30 +0300

    some changes:)

--- src/main/java/com/example/demoregistration/control/SignServlet.java
+++ src/main/java/com/example/demoregistration/control/SignServlet.java
@@ -3,6 +3,9 @@ package com.example.demoregistration.control;
 import com.example.demoregistration.entity.Role;
 import com.example.demoregistration.entity.User;
 import com.example.demoregistration.service.UserService;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,9 +16,16 @@ import java.io.IOException;
 
 @WebServlet("/sign")
 public class SignServlet extends HttpServlet {
+
+    private static final Logger LOGGER = LogManager.getLogger(SignServlet.class);
+    private static final Logger ROOT_LOGGER = LogManager.getRootLogger();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         getServletContext().getRequestDispatcher("/res/sign.jsp").forward(req, resp);
+
+        LOGGER.log(Level.INFO,"warn-error");
+        ROOT_LOGGER.log(Level.INFO,"root_error");
     }
 
     @Override
